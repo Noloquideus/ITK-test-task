9c8164b7ca0194fb93a86fc33bc41f3c1762ec4b
Noloquideus <daniilmanukian@gmail.com>
2025-07-11 18:56:46 +0300

    feat: add pool_size, max_overflow, pool_timeout, pool_recycle, echo

--- src/infrastructure/database/database.py
+++ src/infrastructure/database/database.py
@@ -2,5 +2,12 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from src.settings import settings
 
 
-engine = create_async_engine(settings.DATABASE_URL, pool_size=10, max_overflow=20)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    pool_size=settings.DATABASE_POOL_SIZE,
+    max_overflow=settings.DATABASE_MAX_OVERFLOW,
+    pool_timeout=settings.DATABASE_POOL_TIMEOUT,
+    pool_recycle=settings.DATABASE_POOL_RECYCLE,
+    echo=False
+)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
