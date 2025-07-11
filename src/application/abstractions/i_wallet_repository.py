115bb7a90b6c66a83ce193579ba4abc930d94af6
Noloquideus <daniilmanukian@gmail.com>
2025-07-11 12:49:23 +0300

    feat: add wallet repository interface

---
+++ src/application/abstractions/i_wallet_repository.py
@@ -0,0 +1,29 @@
+from abc import abstractmethod
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.application.contracts.i_wallet_service import IWalletService
+from src.infrastructure.database.models.wallet import Wallet
+from src.infrastructure.logger import Logger
+
+
+class IWalletRepository(IWalletService):
+
+    def __init__(self, session: AsyncSession, logger: Logger):
+        self._session = session
+        self._logger = logger
+
+
+    @abstractmethod
+    async def create(self) -> Wallet:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def deposit(self, wallet_id: str, value: float) -> Wallet:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def withdraw(self, wallet_id: str, value: float) -> Wallet:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_wallet(self, wallet_id: str) -> Wallet:
+        raise NotImplementedError
